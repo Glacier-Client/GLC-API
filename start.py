96697721bd22b0c71553fbe14b6b59a5718b3e92
SpyMIner <vince051105@icloud.com>
2022-10-13 23:48:07 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,12 @@
 import os
 print("checking installed:")
 os.system("apt install python3-pip -y")
+os.system("apt install git -y")
 print("Starting update:")
 print("Pulling git:")
 os.system("git pull")
 print("installing requirements:")
 os.system("pip install -r requirements.txt")
+os.system("apt install uvicorn -y")
 print("starting server:")
 os.system("uvicorn main:app --reload --host 0.0.0.0 --port 8083")
